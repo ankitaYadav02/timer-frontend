5cb90384ab8721d565dfdb5677719374ca6b8e9c
ankitaYadav02 <ankitarao01011998@gmail.com>
2022-04-05 23:49:51 +0530

    Add custom hooks

--- src/components/Home/useTimerPage.ts
+++ src/components/Home/useTimerPage.ts
@@ -1,28 +1,30 @@
-import { useState, ChangeEvent } from 'react';
-import { convertDurationToSeconds } from 'utils/constants';
+import { useState, ChangeEvent } from "react";
+import { convertDurationToSeconds } from "utils/constants";
 
 const useTimerPage = () => {
-    const [ duration, setDuration ] = useState({
-        hour: 0,
-        minute: 0,
-        second: 0
-    })
+	const [duration, setDuration] = useState({
+		hour: 0,
+		minute: 0,
+		second: 0,
+	});
 
-    const onChangeDuration = (e: ChangeEvent<HTMLInputElement>) => {
-        const { target: { name, value }} = e;
-            setDuration((prev) => ({ ...prev, [name]: value }))
-    }
+	const onChangeDuration = (e: ChangeEvent<HTMLInputElement>) => {
+		const {
+			target: { name, value },
+		} = e;
+		setDuration((prev) => ({ ...prev, [name]: value }));
+	};
 
-    const createTimer = () => {
-        const d = convertDurationToSeconds(duration);
-        console.log(d,"timer created");
-    }
+	const createTimer = () => {
+		const d = convertDurationToSeconds(duration);
+		console.log(d, "timer created");
+	};
 
-    return { 
-        duration,
-        onChangeDuration,
-        createTimer
-    }
-}
+	return {
+		duration,
+		onChangeDuration,
+		createTimer,
+	};
+};
 
 export default useTimerPage;
